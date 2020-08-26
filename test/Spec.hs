0765ed9232cffc6bacd8b0a12e31c02f9ad1489c
laughinginloud <mattia.martelli@live.it>
2020-08-26 15:19:01 +0200

    Modifiche a testMonoide e commenti Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,6 +6,9 @@ main =
     testMonoide
     >> colorStrLn Vivid Yellow "\nFine dei test"
 
+
+----------- Colori
+
 colorStrLn :: ColorIntensity -> Color -> String -> IO ()
 colorStrLn fgi fg str = do
     setSGR [SetColor Foreground fgi fg]
@@ -19,12 +22,19 @@ corretto = colorStrLn Vivid Green
 errato :: String -> IO ()
 errato = colorStrLn Vivid Red
 
----- TEST
+----------- Fine colori
+
+
+----------- Test
 
 testMonoide :: IO ()
 testMonoide =
-    case filter (\x -> (x <> mempty /= x) || (mempty <> x /= x)) [min..max] of
+    case filter controllo [min..max] of
         [] -> corretto "Test monoide passato"
         _  -> errato "Test monoide non passato"
+
     where min = fromInteger (-100) :: Intero
           max = fromInteger 100 :: Intero
+          controllo = \x -> (x <> mempty /= x) || (mempty <> x /= x)
+
+----------- Fine test

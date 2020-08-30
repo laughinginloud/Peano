7b58b72c474117d9f606d9b2756eac9593f42348
laughinginloud <mattia.martelli@live.it>
2020-08-30 23:58:58 +0200

    Implementazione enum, normalizzazioni, spaziatura

--- src/PeanoQ.hs
+++ src/PeanoQ.hs
@@ -11,12 +11,12 @@ data Razionale = Intero :/ Intero
 
 normalizza :: Razionale -> Razionale
 normalizza (x :/ y)
-    | y < 0 = (negate x) :/ (negate y) |> normalizza
+    | y < 0     = (negate x) :/ (negate y) |> normalizza
     | otherwise = x' :/ y'
         where
-            mcm = gcd x y |> toInteger .> fromInteger
-            x' = x |> toInteger .> fromInteger |> flip (/) mcm |> truncate .> fromInteger
-            y' = y |> toInteger .> fromInteger |> flip (/) mcm |> truncate .> fromInteger
+            mcd = gcd x y |> toInteger
+            x'  = x |> toInteger |> flip quot mcd |> fromInteger
+            y'  = y |> toInteger |> flip quot mcd |> fromInteger
 
 
 instance Ord Razionale where
@@ -24,35 +24,42 @@ instance Ord Razionale where
 
 instance Num Razionale where
     (+) (x :/ y) (x' :/ y')
-        | y == y'   = (x + x') :/ y |> normalizza
-        | otherwise = ((x * y') + (x' * y)) :/ (lcm y y') |> normalizza
+        | y == y'   = (x + x') :/ y
+        | otherwise = ((x * y') + (x' * y)) :/ (lcm y y')
+        |> normalizza
 
     (*) (x :/ y) (x' :/ y') = (x * x') :/ (y * y') |> normalizza
 
-    abs (x :/ y)
-        | x < 0     = (negate x) :/ y
-        | otherwise = x :/ y
+    abs = normalizza .> abs'
+        where
+            abs' (x :/ y)
+                | x < 0     = (negate x) :/ y
+                | otherwise = x :/ y
 
-    signum (x :/ y)
-        | x == 0 = 0
-        | x < 0 = -1
-        | otherwise = 1
+    signum = normalizza .> signum'
+        where
+            signum' (x :/ y)
+                | x == 0    = 0
+                | x < 0     = -1
+                | otherwise = 1
 
     fromInteger x = (fromInteger x) :/ 1
 
     negate (x :/ y) = (negate x) :/ y |> normalizza
 
---instance Enum Razionale where
+instance Enum Razionale where
+    toEnum = toInteger .> fromInteger
+
+    fromEnum (x :/ y) = quot x y |> toInt
+        where toInt = toInteger .> fromInteger
 
 instance Real Razionale where
     toRational (x :/ y) = (toInteger x) :% (toInteger y)
 
---instance Integral Razionale where
-
 instance Fractional Razionale where
     fromRational (x :% y) = (fromInteger x) :/ (fromInteger y)
 
-    recip (x :/ y) = y :/ x
+    recip (x :/ y) = y :/ x |> normalizza
 
 instance Semigroup Razionale where
     (<>) = (+)
